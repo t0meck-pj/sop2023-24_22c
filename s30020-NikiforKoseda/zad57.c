d8ccd1d817c4d7e63aacabcf1e38d41c17ba727b
NK4 <s30020@pjwstk.edu.pl>
2024-06-03 18:35:33 +0200

    Ostatnie zadanie z działu 5

---
+++ s30020-NikiforKoseda/zad57.c
@@ -0,0 +1,62 @@
+/*Napisz program, który wygeneruje drzewko procesów wyglądające tak (oczywiście procesy nie będą się nazywały A, B, ...):
+
+A \
+
+  |\B \
+
+  |   |\D
+
+  |    \E
+
+   \C \
+
+      |\F
+
+       \G
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(){
+    pid_t pid;
+    pid = fork();
+    if(pid == 0){
+        pid = fork();
+        if(pid == 0){
+            pid = fork();
+            if(pid == 0){
+                printf("G\n");
+            }
+            else{
+                wait(NULL);
+                printf("F\n");
+            }
+        }
+        else{
+            wait(NULL);
+            pid = fork();
+            if(pid == 0){
+                printf("E\n");
+            }
+            else{
+                wait(NULL);
+                printf("D\n");
+            }
+        }
+    }
+    else{
+        wait(NULL);
+        pid = fork();
+        if(pid == 0){
+            printf("C\n");
+        }
+        else{
+            wait(NULL);
+            printf("A\n");
+        }
+    }
+    return 0;
+}

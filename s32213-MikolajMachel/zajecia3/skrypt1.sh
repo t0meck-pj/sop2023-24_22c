116796e6ebdab3bb4d794fad04e1ea821afc64d1
Mikołaj Machel <s32213@szuflandia22.pjwstk.edu.pl>
2024-06-04 22:55:01 +0200

    zajecia 3

---
+++ s32213-MikolajMachel/zajecia3/skrypt1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Sprawdzanie czy podało więcej niż jeden parametr
+if [ $# -gt 1 ]; then
+    echo "Pierwszy parametr: $1"
+elif [ $# -eq 1 ]; then
+    echo "Tylko jeden parametr został podany: $1"
+else
+    echo "Nie podano żadnych parametrów."
+
+
